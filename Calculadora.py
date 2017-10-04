baf561f680ae03d7a8c059a5d1c03957db5123ab
Luis Cristerna <luis.cristerna14@gmail.com>
2017-10-03 22:19:30 -0500

    Prueba 1

---
+++ Calculadora.py
@@ -0,0 +1,58 @@
+# Luis Manuel Cristerna Gallegos 05/09/2017
+# Implementando CI con travis
+#prueba 5
+import math
+
+
+class Calculadora():
+
+    def __init__(self):
+        self.resultado = 0
+
+    def obtener_resultado(self):
+        return self.resultado
+
+    def suma(self, num1, num2):
+        try:
+            self.resultado = num1 + num2
+        except:
+            self.resultado = "Datos Incorrectos"
+
+    def resta(self, num1, num2):
+        try:
+            self.resultado = num1 - num2
+        except:
+            self.resultado = "Datos Incorrectos"
+
+    def multiplicacion(self, num1, num2):
+        try:
+            self.resultado = num1 * num2
+        except:
+            self.resultado = "Datos Incorrectos"
+
+    def division(self, num1, num2):
+        try:
+            if(num2 == 0):
+                self.resultado = 'No se puede divir entre cero'
+            else:
+                self.resultado = num1 / num2
+        except:
+            self.resultado = 'Datos Incorrectos'
+
+    def potencia(self, num1, num2):
+        try:
+            if(num2 == 0):
+                self.resultado = 'Todo numero elevado a la potencia cero es uno'
+            else:
+                self.resultado = num1 ** num2
+        except:
+            self.resultado = 'Datos Incorrectos'
+
+    def raiz(self, num1):
+        try:
+            if(num1 < 0):
+                self.resultado = 'No se puede realizar raiz cuadrada con numeros negativos'
+            else:
+                self.resultado = math.sqrt(num1)
+        except:
+            self.resultado = 'Datos incorrectos'
